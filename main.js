07df51d01bae7056f328f61070cb67fa4497ea37
Vivian <b05901179@ntu.edu.tw>
2020-10-06 08:47:53 +0800

    all

---
+++ main.js
@@ -0,0 +1,22 @@
+var cells = document.getElementsByClassName("date")
+var input = document.getElementById("cal-input")
+var color = document.getElementById("cal-color")
+var button = document.getElementById("cal-button")
+input.value = ''
+color.value = '#b0b0b0'
+
+
+
+
+
+
+
+
+
+//Sets the page's theme. No need to modify
+var themeButton = document.getElementsByClassName("ChooseTheme")
+for(var i=0; i<themeButton.length; ++i) {
+    themeButton[i].addEventListener('click', e => {
+        document.body.setAttribute('class', e.target.id)
+    }, false)
+}
